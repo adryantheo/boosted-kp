0ac3c6737323db8c7cf71c412b84b5ad81a3f00b
A.Theo <adryantheo@gmail.com>
2019-03-27 00:04:26 +0700

    Fix Get Nota

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -32,7 +32,7 @@ class NotaController extends Controller
     
     public function show(Nota $nota)
     {
-        return response()->json($nota->with('Order')->whereIn('id', $nota)->get(),200);     
+        return response()->json($nota::with('order')->where('id', '=', $nota->id)->first());     
     }
 
    
