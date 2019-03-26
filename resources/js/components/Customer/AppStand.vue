cd3e6cc4debf51e695045d8ec1c99f2ad84c6f01
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-03-26 21:36:17 +0700

    added stand detail api

--- resources/js/components/Customer/AppStand.vue
+++ resources/js/components/Customer/AppStand.vue
@@ -8,34 +8,33 @@
         </v-parallax>
 
         <v-container grid-list-lg class="my-3">
-            <div class="headline font-weight-bold mb-1">Stand {{ stand }}</div>
-            <p class="subheading">Lokasi 1</p>
+            <div class="headline font-weight-bold mb-1">Stand {{ name }}</div>
+            <p class="subheading">{{ description }}</p>
             <v-divider class="my-4"></v-divider>
 
             <p class="subheading font-weight-bold">
                 Daftar Menu
             </p>
             <v-layout row wrap>
-                <v-flex xs12 md4 lg3  v-for="(item, i) in allMenus" :key="`am-${i}`">
-                    <v-card class="rounded">
+                <v-flex xs12 md4 lg3  v-for="(item, i) in standProducts" :key="`am-${i}`">
+                    <v-card class="rounded" height="100%">
                         <v-img
-                        :src="item.thumb"
-                        aspect-ratio="1"
-                        height="200"
+                        :src="item.image"
+                        :aspect-ratio="16/9"
                         ></v-img>
 
-                        <v-card-title>
-                            <div>
-                                <span class="headline">{{ item.name }}</span>
-                                <div class="grey--text text--darken-2">
-                                    <span>{{ $rupiahFormat(item.price) }}</span>
-                                </div>
-                            </div>
-                        </v-card-title>
+                        <v-card-text>
+                            <p class="title font-weight-regular">{{ item.name }}</p>
+                            <div class="subheading">{{ $rupiahFormat(item.price) }}</div>
+                            <div class="subheading">Sisa {{ item.units }}</div>
+                        </v-card-text>
 
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn flat color="primary" v-show="!item.qty" @click="addToCart(item)">Tambah</v-btn>
+                            <v-btn color="primary" round flat v-show="!item.qty" @click="addToCart(item)">
+                                <v-icon left>add_shopping_cart</v-icon>
+                                tambah
+                            </v-btn>
                             <div v-show="item.qty">
                                 <v-layout align-center>
                                     <v-flex>
@@ -50,7 +49,7 @@
                                     <v-flex>
                                         <v-btn icon outline color="primary" 
                                         @click="addToCart(item)" 
-                                        :disabled="item.qty >= item.unit">
+                                        :disabled="item.qty >= item.units">
                                             <v-icon>add</v-icon>
                                         </v-btn>
                                     </v-flex>
@@ -62,23 +61,6 @@
             </v-layout>
         </v-container>
 
-        <v-bottom-sheet v-model="sheet" inset>
-            <v-card>
-                <v-card-text class="text-xs-center">
-                    <v-img center
-                    src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-                    :aspect-ratio="1" height="500" width="500"
-                    ></v-img>
-                </v-card-text>
-                
-                <v-card-text class="text-xs-center">
-                    <v-btn large round color="primary">
-                        <v-icon left>shopping_basket</v-icon>
-                        tambah ke keranjang
-                    </v-btn>
-                </v-card-text>
-            </v-card>
-        </v-bottom-sheet>
     </div>
 </template>
 <script>
@@ -98,39 +80,39 @@ export default {
     },
     data() {
         return {
-            sheet: false,
-
-            allMenus: [
-                {
-                    id: 1,
-                    thumb: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
-                    name: "Menu 1",
-                    price: 10000,
-                    unit: 10,
-                },
-                {
-                    id: 2,
-                    
-                    thumb: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
-                    name: "Menu 2",
-                    price: 15000,
-                    unit: 10,
-                },
-                {
-                    id: 3,
-                    thumb: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
-                    name: "A",
-                    price: 14000,
-                    unit: 10,
-                },
-            ],
+            name: "",
+            description: "",
+            standProducts: [],
         }
+    },
+    computed: {
+        ...mapGetters([
+            'getCartItems'
+        ]),
     },
     methods: {
         ...mapMutations({
             addToCartVuex: 'addToCart',
             removeFromCartVuex: 'removeFromCart',
         }),
+        async fetchStandDetails() {
+            const res = await axios.get(`/api/stands/${this.stand}`);
+            const stand = res.data[0];
+            this.name = stand.name;
+            this.description = stand.description;
+            this.standProducts = stand.products.map((item) => {
+                let q = 0;
+                this.getCartItems.forEach(cartItem => {
+                    if(cartItem.id === item.id) {
+                        q = cartItem.qty
+                    }
+                });
+                return {
+                    ...item, 
+                    qty: q
+                }
+            });
+        },
         addToCart(item) {
             // add item qty in this component
             item.qty++;
@@ -144,34 +126,8 @@ export default {
             this.removeFromCartVuex(item);
         },
     },
-    computed: {
-        ...mapGetters([
-            'getCartItems'
-        ]),
-    },
     mounted() {
-        if(!!this.item) {
-            this.sheet = true
-        }
-
-        this.getCartItems.map((item) => item.qty)
-        
-        this.allMenus = this.allMenus.map((item) => {
-            let q = 0;
-            this.getCartItems.forEach(cartItem => {
-                if(cartItem.id === item.id) {
-                    q = cartItem.qty
-                }
-            });
-            return {
-                ...item, 
-                qty: q
-            }
-        })
-        this.allMenus.forEach(e => {
-            console.log(e.name, e.qty);
-        });
-        
+        this.fetchStandDetails();
     }
 }
 </script>
