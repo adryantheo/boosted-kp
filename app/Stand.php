02d904393ea7b08a32b7bcf65a2c23f7170a65a7
A.Theo <adryantheo@gmail.com>
2019-03-26 19:36:55 +0700

    Cleaning Comments Stand Model

--- app/Stand.php
+++ app/Stand.php
@@ -17,34 +17,5 @@ class Stand extends Model
         return $this->hasMany(Product::class);
     }
 
-    // public function ProductList(){
-
-    //     $products = [];
-
-    //     foreach ($this->Products as $product){
-
-    //         $products[]= $product->with([
-    //         ])
-    //         ->get();
-        
-    //     }
-    //     return $products;
-
-    // }
+   
 }
-
-
-
-// public function outletList()
-//     {
-//         $outlets = [];
-
-//         foreach ($this->outlets as $outlet) {
-//             $outlets[] = $outlet->with(['addons:outlet_id,addon_id,status', 'addons.addon:id,name', 'cashiers:id,name,pin,outlet_id', 'outletType:id,name'])
-//                             ->where('id', '=', $outlet->id)
-//                             ->select('id', 'name', 'alias', 'address', 'logo', 'footer', 'contact', 'tagline', 'prefix', 'start_subscription', 'end_subscription', 'outlet_type_id')
-//                             ->first();
-//         }
-
-//         return $outlets;
-//     }
