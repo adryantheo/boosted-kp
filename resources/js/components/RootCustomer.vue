4543e6ce37d811c56dc2ce543e75d69d9e703b15
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-03-29 16:25:10 +0700

    made cart dialog to v-window

--- resources/js/components/RootCustomer.vue
+++ resources/js/components/RootCustomer.vue
@@ -23,11 +23,14 @@
                         Keranjang anda
                     </div>
                     <v-spacer></v-spacer>
-                    <v-btn icon @click="cartDialog = false">
+                    <v-btn icon @click="closeDialog">
                         <v-icon>close</v-icon>
                     </v-btn>
                 </v-toolbar>
-                <v-card-text>
+
+                <v-window v-model="step">
+                <v-window-item :value="1">
+                    <v-card-text>
                     <v-layout column>
                         <v-flex v-for="(item, i) in getCartItems" :key="`cartItemsName-${i}`" class="subheading mb-2">
                         <v-layout row wrap>
@@ -54,38 +57,72 @@
                         </v-layout>
                         </v-flex>
                     </v-layout>
-                </v-card-text>
+                    </v-card-text>
+                </v-window-item>
+
+                <v-window-item :value="2">
+                    <v-card-text>
+                    <v-text-field
+                        v-model="ordererName"
+                        label="Nama anda"
+                        type="text"
+                        ref="orderer_name"
+                    ></v-text-field>
+                    </v-card-text>
+                </v-window-item>
+                </v-window>
+
                 <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn large color="green" dark>
-                        <v-icon>attach_money</v-icon>
-                        pesan
-                    </v-btn>
+                <v-btn
+                    :disabled="step === 1"
+                    large flat
+                    @click="step--"
+                >
+                    Kembali
+                </v-btn>
+                <v-spacer></v-spacer>
+                <v-btn
+                    v-if="step === 2" large
+                    color="success" @click="makeOrder(ordererName)"
+                    :disabled="!ordererName"
+                >
+                    Pesan
+                </v-btn>
+                <v-btn
+                    v-else large
+                    color="success"
+                    outline @click="toPayment"
+                >
+                    Lanjut
+                </v-btn>
                 </v-card-actions>
+
             </v-card>
         </v-dialog>
+
         <v-fab-transition>
-        <v-btn
-            color="primary"
-            fab dark large
-            bottom right fixed
-            @click="cartDialog = true"
-            v-show="!!getCartItems.length"
-        >
-            <v-icon>shopping_cart</v-icon>
-        </v-btn>
+            <v-btn
+                color="primary"
+                fab dark large
+                bottom right fixed
+                @click="cartDialog = true"
+                v-show="!!getCartItems.length"
+            >
+                <v-icon>shopping_cart</v-icon>
+            </v-btn>
         </v-fab-transition>
     </v-app>
 </template>
 <script>
 import { mapGetters } from 'vuex'
+import { mapActions } from 'vuex'
 
 export default {
-    data() {
-        return {
-            cartDialog: false,
-        }
-    },
+    data: () => ({
+        cartDialog: false,
+        step: 1,
+        ordererName: "",
+    }),
     computed: {
         ...mapGetters([
             'getCartItems'
@@ -95,5 +132,20 @@ export default {
             return total
         }
     },
+    methods: {
+        ...mapActions([
+            'makeOrder'
+        ]),
+        closeDialog() {
+            this.cartDialog = false;
+            this.step = 1;
+        },
+        toPayment() {
+            this.step++;
+            setTimeout(() => {
+                this.$refs.orderer_name.focus();
+            }, 300);
+        },
+    }
 }
 </script>
