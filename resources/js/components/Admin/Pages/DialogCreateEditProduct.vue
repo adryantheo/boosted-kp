b604521a0a28c0da5b6aa5205ba2d273f8b2938b
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-03-30 21:57:26 +0700

    change dialog title on create/edit

--- resources/js/components/Admin/Pages/DialogCreateEditProduct.vue
+++ resources/js/components/Admin/Pages/DialogCreateEditProduct.vue
@@ -1,7 +1,9 @@
 <template>
     <v-card>
         <v-toolbar color="transparent" flat>
-            <v-toolbar-title class="headline">Menu Baru</v-toolbar-title>
+            <v-toolbar-title class="headline">
+                {{ !!productId? 'Ubah Menu' : 'Menu Baru'}}
+            </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn icon @click="$emit('close')">
                 <v-icon>close</v-icon>
--- resources/js/components/Admin/Pages/DialogCreateEditStand.vue
+++ resources/js/components/Admin/Pages/DialogCreateEditStand.vue
@@ -1,7 +1,9 @@
 <template>
     <v-card>
         <v-toolbar color="transparent" flat>
-            <v-toolbar-title class="headline">Stand Baru</v-toolbar-title>
+            <v-toolbar-title class="headline">
+                {{ !!standId? 'Ubah Stand' : 'Stand Baru'}}
+            </v-toolbar-title>
             <v-spacer></v-spacer>
             <v-btn icon @click="$emit('close')">
                 <v-icon>close</v-icon>
