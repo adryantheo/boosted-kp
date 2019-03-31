4c30b98ca8174505c7942ab6355e775ffb884a7f
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2019-04-01 04:04:51 +0700

    added fetching transaction data

--- resources/js/components/Admin/Pages/AppAdminTransaction.vue
+++ resources/js/components/Admin/Pages/AppAdminTransaction.vue
@@ -13,47 +13,165 @@
         <template v-else>
         <v-layout row wrap>
             <v-flex xs12>
-            <v-layout justify-space-between align-center>
+            <v-layout align-center>
                 <v-flex>
                     <p class="headline primary--text">Semua Transaksi</p>
                 </v-flex>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="getAllOrders">
+                    <v-icon left>replay</v-icon>
+                    muat ulang
+                </v-btn>
+                <v-btn color="success" @click="printOrders">
+                    <v-icon left>print</v-icon>
+                    cetak
+                </v-btn>
             </v-layout>
             </v-flex>
-            
+            <v-flex xs12>
+                <v-card>
+                    <v-container grid-list-lg>
+                    <v-layout row wrap>
+                        <v-flex xs12 md6>
+                            <v-card color="blue-grey darken-3" dark>
+                                <v-card-text>
+                                    <p class="subheading">Total Produk Terjual</p>
+                                    <div class="headline">
+                                        {{ getTotalSold }} item
+                                    </div>
+                                </v-card-text>
+                            </v-card>
+                        </v-flex>
+                        <v-flex xs12 md6>
+                            <v-card color="blue-grey darken-3" dark>
+                                <v-card-text>
+                                    <p class="subheading">Total Pendapatan</p>
+                                    <div class="headline">
+                                        {{ $rupiahFormat(getTotalEarnings) }}
+                                    </div>
+                                </v-card-text>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+                    <v-data-table
+                        :headers="headers"
+                        :items="items"
+                        disable-initial-sort
+                        :loading="loading"
+                        rows-per-page-text="Baris per halaman"
+                    >
+                        <template v-slot:items="props">
+                            <td>{{ props.item.id }}</td>
+                            <td>{{ props.item.date }}</td>
+                            <td>{{ props.item.menu }}</td>
+                            <td>{{ props.item.customer }}</td>
+                            <td class="text-xs-right">{{ props.item.qty }}</td>
+                            <td class="text-xs-right">{{ $rupiahFormat(props.item.price) }}</td>
+                            <td class="text-xs-right">{{ $rupiahFormat(props.item.total) }}</td>
+                        </template>
+                    </v-data-table>
+                </v-card>
+            </v-flex>
         </v-layout>
         </template>
-        <v-dialog
-            v-model="dialogTransactionDetail"
-            persistent max-width="600px"
-        >
-            <dialog-transaction-detail @close="dialogTransactionDetail = false"></dialog-transaction-detail>
-        </v-dialog>
+
+        <div v-show="false" id="printMe">
+            <div class="ma-3">
+                <div class="text-xs-center">
+                    <p class="headline">Laporan Riwayat Transaksi</p>
+                    <p class="title">Seluruh Stand</p>
+                </div>
+                <div>
+                    <table class="orders">
+                        <tr>
+                            <th>Id order</th>
+                            <th>Tanggal order</th>
+                            <th>Menu</th>
+                            <th>Pelanggan</th>
+                            <th>Jumlah</th>
+                            <th>Harga</th>
+                            <th>Total</th>
+                        </tr>
+                        <template v-for="(item, i) in items">
+                            <tr :key="i">
+                                <td>{{ item.id }}</td>
+                                <td>{{ item.date }}</td>
+                                <td>{{ item.menu }}</td>
+                                <td>{{ item.customer }}</td>
+                                <td class="text-xs-right">{{ item.qty }}</td>
+                                <td class="text-xs-right">{{ $rupiahFormat(item.price) }}</td>
+                                <td class="text-xs-right">{{ $rupiahFormat(item.total) }}</td>
+                            </tr>
+                        </template>
+                        <tfoot class="title">
+                            <tr>
+                                <td class="text-xs-right" colspan="6">Total produk terjual</td>
+                                <td>{{ getTotalSold }} item</td>
+                            </tr>
+                            <tr>
+                                <td class="text-xs-right" colspan="6">Total pendapatan</td>
+                                <td>{{ $rupiahFormat(getTotalEarnings) }}</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
     </v-container>
 </template>
 <script>
 export default {
     data: () => ({
         loading: false,
-        transactions: [],
+        headers: [
+            { text: 'ID', value: 'id', sortable: false },
+            { text: 'Tgl order', value: 'date' },
+            { text: 'Nama menu', value: 'menu', sortable: false  },
+            { text: 'Pelanggan', value: 'customer', sortable: false },
+            { text: 'Jumlah', value: 'qty' },
+            { text: 'Harga', value: 'price' },
+            { text: 'Total', value: 'total' },
+        ],
+        items: [],
         dialogTransactionDetail: false,
     }),
+    computed: {
+        getTotalSold() {
+            return this.items.reduce((acc, item) => acc + item.qty, 0);
+        }, 
+        getTotalEarnings() {
+            return this.items.reduce((acc, item) => acc + item.total, 0);
+        }, 
+    },
     methods: {
-        fetchTransactions() {
-            // return axios.get('/api/transactions')
+        fetchAllOrders() {
+            return axios.get('/api/orders');
         },
-        async getTransactions() {
+        async getAllOrders() {
             this.loading = true;
             try {
-                const res = await this.fetchTransactions();
-                this.transactions = res.data;
+                const res = await this.fetchAllOrders();
+                this.items = res.data.map(item => ({
+                    id: item.id,
+                    date: item.created_at,
+                    menu: item.product.name,
+                    customer: item.nota.customer,
+                    price: item.harga_satuan,
+                    qty: item.quantity,
+                    total: (item.quantity * item.harga_satuan)
+                }));
             } catch (err) {
                 console.log(err);
             }
             this.loading = false;
         },
+        printOrders() {
+            this.$htmlToPaper('printMe');
+        },
     },
     mounted() {
-        // this.getTransactions();
+        this.getAllOrders();
     }
 }
 </script>
